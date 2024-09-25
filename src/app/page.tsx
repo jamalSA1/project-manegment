94f7966b759317f8abe583c23881011ba3ea5c87
jamalSA1 <m0716319@gmail.com>
2024-09-25 21:46:43 +0300

    Database connection done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
+import { db } from "~/server/db";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
@@ -8,10 +9,12 @@ export default async function Home() {
 
   void api.post.getLatest.prefetch();
 
+  const posts = await db.post.findMany()
+
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        project manegment website
+        <LatestPost />
       </main>
     </HydrateClient>
   );
