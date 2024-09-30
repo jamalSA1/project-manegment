81a6331ffcb7e2bdd6df11d84d21b251a34167b1
jamalSA1 <m0716319@gmail.com>
2024-09-30 10:29:09 +0300

    rename the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <MaxWidthWrapper>
       <div className="flex justify-between items-center bg-zinc-50/70 p-3 mt-3 rounded-2xl sticky shadow-sm border border-gray-200">
         <Link href='/' className='flex justify-center items-center'>
-        <img src="/logo-01.png" alt="logo" className='w-8 h-8 mr-1' />
+        <img src="/Logo-01.png" alt="logo" className='w-8 h-8 mr-1' />
         <span className='text-2xl bg-gradient-to-t from-gray-300 via-gray-600 to-gray-950 bg-clip-text text-transparent'>Ruill</span>
         </Link>
 
